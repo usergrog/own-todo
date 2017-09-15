9faf5c7e68687d7899cc127fc10a8478df53489b
Alexey Sidorenko <sidorenko.al@gmail.com>
2017-09-15 09:25:41 +0300

    refactoring FirebaseTodoList

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReduxFirebaseTodoList from "../containers/ReduxFirebaseTodoList";
+import FirebaseTodoList from "../firebase/FirebaseTotoList";
+
 
 export const Home = (props) => {
     console.log('home',props)
     return (
         <div>
-            {props.authReducer.userId && <ReduxFirebaseTodoList/>}
+            {props.authReducer.userId && <FirebaseTodoList/>}
             {!props.authReducer.userId && <h1>Please log in</h1>}
         </div>
     )
