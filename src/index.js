fd0d2102decc43619029a2824d91e372e5cfde52
Alexey Sidorenko <sidorenko.al@gmail.com>
2017-09-15 10:51:15 +0300

    index refactoring

--- src/index.js
+++ src/index.js
@@ -4,23 +4,8 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux'
-import {ConnectedRouter, routerMiddleware, routerReducer} from 'react-router-redux'
-import fire from "./firebase/fire";
-import {successLogin} from "./actions/authActions";
-import {push} from 'react-router-redux'
-import {fetchTodos} from "./actions/todoActions";
+import {ConnectedRouter} from 'react-router-redux'
 import {history, store} from "./store";
-
-fire.auth().onAuthStateChanged((user) => {
-    if (user) {
-        console.log('currentUser', user)
-        store.dispatch(successLogin(user.email, '', user.uid))
-        store.dispatch(push('/'));
-        store.dispatch(fetchTodos())
-    } else {
-        console.log('currentUser is empty')
-    }
-});
 
 const render = Component => {
     ReactDOM.render(
